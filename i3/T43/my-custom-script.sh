030273206af19058d4601469def5bffa54ca853a
eam-00 <estebanmontagut@hotmail.com>
2020-05-12 00:05:32 -0300

    Edited out the comments

--- i3/T43/my-custom-script.sh
+++ i3/T43/my-custom-script.sh
@@ -5,18 +5,9 @@
 i3status | while :
 do
  read line
-## weather=`wget -q -O- http://servicios.lanacion.com.ar/solo-texto | grep -e 'Temperatura' | sed 's/^ *//'`
-## weather=`wget -q -O- http://servicios.lanacion.com.ar/solo-texto | grep -e 'Temperatura'`
-## weather=`cat /tmp/lnol.tmp | grep -e 'Temperatura' | sed 's/^ *//'`
-## weather=`cat /tmp/lnol.tmp | grep -e 'Temperatura' | awk '{print "Bs As:"; print $2; print $4; print $5}' | paste -sd " "`
-## weather=`cat /tmp/lnol.tmp | grep -e 'Temperatura'| awk '{print $2}' | paste -sd " "`
-
-## weather=`cat /tmp/lnol.tmp | grep -e 'Temperatura'| awk '{print $1; print $2; print $3; print $4}' | paste -sd " "`
  weather=`cat /tmp/lnol.tmp | grep -e 'Temperatura'| awk '{print $2; print $4}' | sed 's/.$//' | paste -sd " "`
  mem=`free -h | awk '/^Mem:/ {print $3 "/" $2}'`
-##  echo " $weather |  $mem | $line" || exit 1
  echo "  |  $mem | $line" || exit 1
-
 done
 
 ## EoF ##
